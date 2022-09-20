17a0d78708c5b61dfacea9818d164c78c6c92b17
santu2k <114009450+santu2k@users.noreply.github.com>
2022-09-20 11:08:22 -0500

    Add files via upload

---
+++ BorrarArchivos.bat
@@ -0,0 +1,6 @@
+@echo off
+DEL /F /A D:\user\ISER2022\desktop\Archivos\Archivo1.txt
+DEL /F /A D:\user\ISER2022\desktop\Archivos\Archivo2.txt
+DEL /F /A D:\user\ISER2022\desktop\Archivos\Archivo3.txt
+DEL /F /A D:\user\ISER2022\desktop\Archivos\Archivo4.txt
+pause
---
+++ CopiarArchivos.bat
@@ -0,0 +1,3 @@
+@echo off
+xcopy D:\user\ISER2022\desktop\Archivos D:\backup /E /H /C /I
+pause
